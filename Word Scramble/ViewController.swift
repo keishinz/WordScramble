7de5fed7a5b6b3cd50cf86fc5c54fc28479734d4
kesihinz <keishin.chou@gmail.com>
2019-10-27 23:04:22 +0900

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Keishin CHOU on 2019/10/27.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
