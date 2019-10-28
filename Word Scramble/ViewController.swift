b03c53366aa6cbe190def58e9d405d179e0daf10
kesihinz <keishin.chou@gmail.com>
2019-10-28 14:32:02 +0900

    Added anagram functions.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view.
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem  = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -41,7 +42,7 @@ class ViewController: UITableViewController {
         return cell
     }
 
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -69,26 +70,56 @@ class ViewController: UITableViewController {
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(lowerAnswer, at: 0)
 
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .right)
 //                    tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+                } else {
+                    wrongWord(title: "This word does not exist.", message: "Try another word.")
                 }
+            } else {
+                wrongWord(title: "This word is already in the list.", message: "Try another word.")
             }
+        } else {
+            wrongWord(title: "This word contains a letter you used twice.", message: "Try another word.")
         }
     }
     
     func isPossible(word: String) -> Bool {
+        
+        guard var wordsBase = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = wordsBase.firstIndex(of: letter) {
+                wordsBase.remove(at: position)
+            } else {
+                return false
+            }
+        }
         return true
     }
 
     func isOriginal(word: String) -> Bool {
-        return true
+        
+        return !usedWords.contains(word)
     }
 
     func isReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wrongWord(title: String, message: String) {
+
+        let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler:nil)
+        
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
 
 }
