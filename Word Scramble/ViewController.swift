9863bd8a5e2a8e1bbf3cd5e323c7c7a4d64a668a
kesihinz <keishin.chou@gmail.com>
2019-10-28 12:03:22 +0900

    Insert rows in table view.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -64,6 +64,19 @@ class ViewController: UITableViewController {
     
     func submit(submit answer: String) {
         
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .right)
+//                    tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+                }
+            }
+        }
     }
     
     func isPossible(word: String) -> Bool {
